0057242341c3c56b6c87ec2bacfb1ea22815430b
shpark1003-maker <shpark1003@gmail.com>
2026-05-11 01:45:10 +0900

    Refactor: Move static files to root and rename server.js to avoid Vercel 500 conflict

--- server-local.js
+++ server-local.js
@@ -76,7 +76,7 @@ app.use(cors({
 }));
 
 app.use(express.json({ limit: '1mb' }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 const sendError = (res, status, message) => {
     return res.status(status).json({
